d998e46dc7e8f0bd67c3095d77f3ef3194f94f6c
idk <idk@idk.local>
2020-04-08 18:54:59 -0400

    start the actual import package

--- zerobundle/gen/gen.go
+++ zerobundle/gen/gen.go
@@ -7,5 +7,7 @@ import "github.com/zserge/lorca"
 func main() {
 	// You can also run "npm build" or webpack here, or compress assets, or
 	// generate manifests, or do other preparations for your assets.
-	lorca.Embed("zero", "assets.go", "i2p-zero/dist.tar.gz")
+	lorca.Embed("zero", "linux/assets.go", "i2p-zero/dist-zip/i2p-zero-linux.v1.16.zip")
+    lorca.Embed("zero", "mac/assets.go", "i2p-zero/dist-zip/i2p-zero-mac.v1.16.zip")
+    lorca.Embed("zero", "windows/assets.go", "i2p-zero/dist-zip/i2p-zero-windows.v1.16.zip")
 }
