444db8da69de17280a9c0e0edd92e8120f5a1f97
idk <idk@idk.local>
2020-04-10 12:13:22 -0400

    cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,8 @@
-BOOST_ROOT=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/boost \
-    cmake -G "Unix Makefiles" \
-    /media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/i2pd/build \
-    -DBUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/toolchain-mingw.cmake \
-    -DWITH_AESNI=ON -DWITH_UPNP=ON -DWITH_STATIC=ON -DWITH_HARDENING=ON \
-    -DCMAKE_INSTALL_PREFIX:PATH=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/i2pd/i2pd-mingw-64-static \
-    -DZLIB_ROOT=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/stage \
-    -DMINIUPNPC_LIBRARY=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/miniupnpc \
-    -DBOOST_LIBRARYDIR:PATH=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/boost/stage-mingw-64/lib \
-    -DOPENSSL_ROOT_DIR:PATH=/media/longterm/go/src/github.com/eyedeekay/checki2cp/i2pdbundle/stage
+BOOST_ROOT=$WORK_DIR/boost cmake -G 'Unix Makefiles' $WORK_DIR/i2pd/build \
+	-DBUILD_TYPE=Release \
+	-DCMAKE_TOOLCHAIN_FILE=$WORK_DIR/toolchain-mingw.cmake \
+	-DWITH_AESNI=ON -DWITH_STATIC=ON -DWITH_HARDENING=ON -DWITH_LIBRARY=OFF \
+	-DCMAKE_INSTALL_PREFIX:PATH=$WORK_DIR/i2pd/i2pd-mingw-64-static \
+	-DZLIB_ROOT=$WORK_DIR/stage \
+	-DBOOST_LIBRARYDIR:PATH=$WORK_DIR/boost/stage-mingw-64/lib \
+	-DOPENSSL_ROOT_DIR:PATH=$WORK_DIR/stage
