c26a2b76a1a5704ccc790beffb4379bdded978ae
idk <idk@laptop.idk.i2p>
2020-07-24 21:14:22 -0400

    make it run

--- zerobundle/launch.go
+++ zerobundle/launch.go
@@ -1,16 +1,39 @@
 package zerobundle
 
 import (
+	"context"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 )
 
-var command exec.Cmd
+var cmd *exec.Cmd
 
 func GetZeroPID() int {
-	return command.Process.Pid
+	return cmd.Process.Pid
+}
+
+func GetZeroProcess() *os.Process {
+	return cmd.Process
+}
+
+func LatestZero() string {
+	if runtime.GOOS == "windows" {
+		return filepath.Join(LatestZeroBinDir(), "i2p-zero.exe")
+	} else {
+		return filepath.Join(LatestZeroBinDir(), "i2p-zero")
+	}
+}
+
+func LatestZeroJavaHome() string {
+	if runtime.GOOS == "windows" {
+		return filepath.Join(LatestZeroBinDirJavaHome(), "i2p-zero.exe")
+	} else {
+		return filepath.Join(LatestZeroBinDirJavaHome(), "i2p-zero")
+	}
 }
 
 func LatestZeroBinDir() string {
@@ -34,4 +57,62 @@ func LatestZeroBinDirJavaHome() string {
 		log.Fatal(er)
 	}
 	return filepath.Join(JAVA_I2P_OPT_DIR, fs[len(fs)-1].Name(), "router", "bin")
+}
+
+func StopZero() {
+	if runtime.GOOS == "windows" {
+		GetZeroProcess().Signal(os.Kill)
+	} else {
+		GetZeroProcess().Signal(os.Interrupt)
+	}
+}
+
+func CommandZero() (*exec.Cmd, error) {
+	if err := UnpackZero(); err != nil {
+		log.Println(err)
+	}
+	latest := LatestZero()
+	return exec.Command(latest), nil
+}
+
+func CommandZeroContext(ctx context.Context) (*exec.Cmd, error) {
+	if err := UnpackZero(); err != nil {
+		log.Println(err)
+	}
+	latest := LatestZero()
+	return exec.CommandContext(ctx, latest), nil
+}
+
+func RunZero() error {
+	var err error
+	cmd, err = CommandZero()
+	if err != nil {
+		return err
+	}
+	return cmd.Run()
+}
+
+func CommandZeroJavaHome() (*exec.Cmd, error) {
+	if err := UnpackZeroJavaHome(); err != nil {
+		log.Println(err)
+	}
+	latest := LatestZeroJavaHome()
+	return exec.Command(latest), nil
+}
+
+func CommandZeroJavaHomeContext(ctx context.Context) (*exec.Cmd, error) {
+	if err := UnpackZeroJavaHome(); err != nil {
+		log.Println(err)
+	}
+	latest := LatestZeroJavaHome()
+	return exec.CommandContext(ctx, latest), nil
+}
+
+func RunZeroJavaHome() error {
+	var err error
+	cmd, err = CommandZeroJavaHome()
+	if err != nil {
+		return err
+	}
+	return cmd.Run()
 }
---
+++ zerobundle/test/run.go
@@ -0,0 +1,19 @@
+//+build run
+
+package main
+
+import (
+	. "github.com/eyedeekay/checki2cp/zerobundle"
+	"log"
+)
+
+func main() {
+	if err := UnpackZeroJavaHome(); err != nil {
+		log.Println(err)
+	}
+	latest := LatestZeroJavaHome()
+	log.Println("latest zero version is:", latest)
+	if err := RunZeroJavaHome(); err != nil {
+	    log.Fatal(err)
+	}
+}
