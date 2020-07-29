d17124a3349668187373030ab284fbd7eff5d454
idk <idk@laptop.idk.i2p>
2020-07-28 22:09:28 -0400

    readme for zerobundle

--- go-zero/main.go
+++ go-zero/main.go
@@ -12,11 +12,21 @@ func main() {
 	}
 	latest := zerobundle.LatestZero()
 	log.Println("latest zero version is:", latest)
-	if err := zerobundle.StartZero(); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Starting SAM")
-	if err := zerobundle.SAM(); err != nil {
-		log.Fatal(err)
+	if err := WriteI2CPConf(); err != nil {
+		if ok, err := checki2p.ConditionallyLaunchI2P(); ok {
+			if err != nil {
+				log.Println(err)
+			} else {
+				if err := zerobundle.StartZero(); err != nil {
+					log.Fatal(err)
+				}
+				log.Println("Starting SAM")
+				if err := zerobundle.SAM(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		} else {
+			log.Println("Undefined I2P launching error")
+		}
 	}
 }
