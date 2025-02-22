a580f45cfcad48fb74d3da415d88d7d7af4a266e
eyedeekay <hankhill19580@gmail.com>
2025-02-22 17:30:16 -0500

    move a bunch of functions into util

--- util/util.go
+++ util/util.go
@@ -7,6 +7,9 @@ import (
 	"runtime"
 )
 
+// Inithome returns the path to the user's home directory
+// with the given string appended to the end.
+// It panics if the user's home directory cannot be found.
 func Inithome(str string) string {
 	s, e := os.UserHomeDir()
 	if e != nil {
@@ -17,18 +20,24 @@ func Inithome(str string) string {
 	return path
 }
 
+// CheckFileExists checks to see if a file exists at the given path
+// and returns a boolean value.
+// It returns true if the file exists, false otherwise.
 func CheckFileExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
 }
 
+// Home returns the path to the I2P(Java) home directory
+// based on the current operating system.
+// It returns a string containing the path to the I2P home directory.
 func Home() string {
 	if runtime.GOOS == "windows" {
 		return "\\i2p"
 	} else if runtime.GOOS == "darwin" {
 		return "/Library/Application Support/i2p"
 	}
-	return ".i2p"
+	return "/.i2p"
 }
 
 // CheckHeadlessness checks to see if the current environment is headless
